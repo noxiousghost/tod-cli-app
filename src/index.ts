4540aa3b58f9a373a8fedbe1fbc1346eaf510d13
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-11-07 10:13:35 +0545

    setup for making app executable without explicitly using nodejs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('CLI TODO application');
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('TODO CLI APP')
+  .version('0.0.1')
+  .description('Create and view your tasks from the comfort of your terminal');
+program
+  .command('test')
+  .description('Run a test command')
+  .action(() => {
+    console.log('Todo CLI is working!');
+  });
+
+program.parse(process.argv);
+
+//take input from the command line
+// use app -- add
+
+// validate the input
+// if the input is valid save to the json file
+// else display list of error in the input fields
